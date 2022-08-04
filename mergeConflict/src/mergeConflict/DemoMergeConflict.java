bf12c29213d55a7c1965d6311812a8f51f972e66
Mohan-Ramesh <108859804+Mohan-Ramesh@users.noreply.github.com>
2022-08-04 23:31:14 +0530

    Changes done by First Developer

--- mergeConflict/src/mergeConflict/DemoMergeConflict.java
+++ mergeConflict/src/mergeConflict/DemoMergeConflict.java
@@ -3,7 +3,7 @@ package mergeConflict;
 public class DemoMergeConflict {
 	public static void main(String[] args) {
 		
-		int a= 10, b=20;
+		int num1= 10, num2=20;
 		System.out.println(a*b);
 		
 		
